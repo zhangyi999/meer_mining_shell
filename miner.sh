6315c2e2d0de3a2fbde6cc606774305b4049e90e
zhangyi999 <34035751+zhangyi999@users.noreply.github.com>
2021-07-06 11:34:14 +0800

    create files
    add shell

---
+++ miner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+while :
+do
+    echo "***********************mining blake2bd**********************"
+    /root/createBlake2bd.sh
+    echo "***********************mining cuckoo24**********************"
+    /root/createCuckoo24.sh
+done
